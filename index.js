ed7ede78a091669d02f50536cdccca0670817833
LETAIHOANG <hoangle9989n@gmail.com>
2023-03-09 21:06:37 +0900

    [V2.0][Bài tập] 1. Biến, kiểu dữ liệu và toán tử
    Chuyển độ C sang độ F

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ //
+//  let celsius = +prompt('nhap vao 0C')
+//  console.log('celsius', celsius)
+//
+//  let fahrenheit = 32+9*celsius/5
+//  console.log('fahrenheit',fahrenheit)
+//  alert('nhiet do fahrenheit '+fahrenheit)
+//  function chuyenDoiNhietDo() {
+//  let celsius = +document.getElementById('celsius').value;
+// console.log('nhap 0C', celsius)
+//   let fahrenheit =  32+9*celsius/5
+//   console.log('fahrenheit' , fahrenheit)
+//      document.getElementById('fahrenheit').innerHTML = 'nhiet do fahrenhei la:'+fahrenheit
+//  }
+//
+
+
